b58ad61048e76947c662f1ad2103ae8af8c292be
telicoscience <telico@yahoo.com.br>
2024-11-23 10:38:00 -0300

    add: flask-mongoengine

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
 api = Api(app)
+db = MongoEngine(app)
 
+
+# Configurações para o MongoDB
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'users',          # Nome do banco de dados
+    'host': 'mongodb',            # Host do MongoDB
+    'port': 27017,                  # Porta padrão do MongoDB
+    'username': 'admin',      # (Opcional) Usuário para autenticação
+    'password': '12345',        # (Opcional) Senha para autenticação
+    'authentication_source': 'admin'  # (Opcional) Banco de autenticação
+}
 
 class Users(Resource):
     def get(self):
@@ -15,7 +27,7 @@ class User(Resource):
         return {'message': 'user'}
     
     def get(self, cpf):
-        return {'message' : 'cpf'}
+        return {'message' : cpf }
 
 
                 
