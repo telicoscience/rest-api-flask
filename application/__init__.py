b206f025bedd9e50c1957ba262910811e58e844e
telicoscience <telico@yahoo.com.br>
2025-01-10 13:10:40 -0300

    fix issues on environment

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,16 @@
 from flask import Flask
 from flask_restful import Api
 from .db import init_db
-from .app import User, Users, OlaMundo
+from .app import User, Users
 
 
 def create_app(config):
     app = Flask(__name__)
-    api = Api(app)
     app.config.from_object(config)
+    api = Api(app)
     init_db(app)
 
-    # Adicionar os endpoints de acesso
     api.add_resource(Users, '/users')
     api.add_resource(User, '/user', '/user/<string:cpf>')
-    api.add_resource(OlaMundo, '/')
+
     return app
--- config.py
+++ config.py
@@ -1,29 +1,36 @@
 import os
-from mongomock import MongoClient
+
+
+class DevConfig:
+
+    MONGODB_SETTINGS = {
+        'db': os.getenv('MONGODB_DB'),
+        'host': os.getenv('MONGODB_HOST'),
+        'username': os.getenv('MONGODB_USERNAME'),
+        'password': os.getenv('MONGODB_PASSWORD')
+    }
 
 
 class ProdConfig:
-    MONGODB_USERNAME = os.getenv("MONGODB_USERNAME")
-    MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
-    MONGODB_HOST = os.getenv("MONGODB_HOST")
-    MONGODB_DB = os.getenv("MONGODB_DB")
-    MONGODB_SETTINGS = {
-        'host': 'mongodb+srv://%s:%s@%s/%s?retryWrites=true&w=majority&appName=Cluster0'% 
-           (MONGODB_USERNAME, 
-           MONGODB_PASSWORD, 
-           MONGODB_HOST, 
-           MONGODB_DB)
-           }
 
-class DevConfig:
-    MONGODB_USERNAME = os.getenv("MONGODB_USERNAME")
-    MONGODB_PASSWORD = os.getenv("MONGODB_PASSWORD")
-    MONGODB_HOST = os.getenv("MONGODB_HOST")
-    MONGODB_DB = os.getenv("MONGODB_DB")
+    MONGODB_USER = os.getenv('MONGODB_USER')
+    MONGODB_PASSWORD = os.getenv('MONGODB_PASSWORD')
+    MONGODB_HOST = os.getenv('MONGODB_HOST')
+    MONGODB_DB = os.getenv('MONGODB_DB')
+
+    MONGODB_SETTINGS = {
+        'host': 'mongodb+srv://%s:%s@%s/%s?retryWrites=true&w=majority' % (
+          MONGODB_USER,
+          MONGODB_PASSWORD,
+          MONGODB_HOST,
+          MONGODB_DB
+        )
+    }
 
 
 class MockConfig:
+
     MONGODB_SETTINGS = {
         'db': 'users',
-        'mongo_client_class': MongoClient
-           }
+        'host': 'mongomock://localhost'
+    }
